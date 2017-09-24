2781af0b77f8402ba26e9fa0b8820c321365b17b
Daniel Trnka <daniel.trnka@gmail.com>
2017-09-24 16:47:57 +0200

    Parallel version with processess

--- main.c
+++ main.c
@@ -1,47 +1,52 @@
-
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <wait.h>
 #include <stdint.h>
+#include <stdlib.h>
+
+#define FOUND 1 
+#define NOT_FOUND 0
+
 void md5(const uint8_t *initial_msg, size_t initial_len, uint8_t *digest);
 
-int cmp(uint8_t *a, uint8_t *b) {
-	return memcmp(a, b, 16 * sizeof(uint8_t)) == 0;
-}
+char table[255];
 
-// python
-// import string;  string.ascii_letters + string.digits
-const char *letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+int crack(char* str, int len, uint8_t* search) {
+	int done = 0;
+	uint8_t hash[16];
+	do {
+//		printf("%s\n", str);
 
-int crack(char *str, int idx, int len, uint8_t *search) {
-	if (idx < (len - 1)) {
-		for (int c = 0; c < 62; ++c) {
-			str[idx] = letters[c];
-
-			if(crack(str, idx + 1, len, search)) {
-				return 1;
-			}
+		md5((uint8_t*) str, len, hash);
+		if(memcmp(search, hash, 16) == 0) {
+			printf("Found: %s\n", str);
+			return 1;
 		}
-	} else {
-		for (int c = 0; c < 62; ++c) {
-			str[idx] = letters[c];
-
-			uint8_t hash[16];
-			md5((uint8_t*)str, len, hash);
-			if(cmp(search, hash)) {
-				printf("Found: %s\n", str);
-				return 1;
+		int pos = len - 1;
+		do {
+			str[pos] = table[str[pos]];
+			if(str[pos] == '0') {
+				pos--;
+			} else {
+				break;
 			}
-		}
-	}
+
+			if(pos < 0) {
+				done = 1;
+			}
+		} while(pos >= 0);
+
+	} while(!done);
 
 	return 0;
 }
 
+const char letters[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 int main(int argc, char **argv) {
 	if(argc != 3) {
-		printf("Usage: %s hash letter\n", argv[0]);
+		printf("Usage: %s hash passsize\n", argv[0]);
 		exit(1);
 	}
 
@@ -50,13 +55,47 @@ int main(int argc, char **argv) {
 		sscanf(argv[1] + 2*i, "%2x", &search[i]);
 	}
 
+	int len = atoi(argv[2]);
 
-	int size = atoi(argv[2]);
-	printf("Going to crack %d length...\n", size);
+	printf("Going to crack %d length...\n", len);
+	for(int i = 0; i < sizeof(table) - 1; i++) {
+		table[i] = i + 1;
+	}
+	table['9'] = 'A';
+	table['Z'] = 'a';
+	table['z'] = '0';
 
-	char str[size + 1];
-	str[size] = 0;
-	crack(str, 0, size, search);
+
+	char str[len + 1];
+	memset(str, '0', len);
+	str[len] = 0;
+
+	int threads = 4;
+	pid_t running[threads];
+	for(int i = 0; i < threads; i++) {
+		running[i] = fork();
+		if(running[i] == 0) {	
+			str[0] = letters[sizeof(letters) / threads * i];
+			if(crack(str, len, search)) {
+				return 1;
+			}
+			return 0;
+		}
+	}
+
+	int status;
+	pid_t pid;
+	while((pid = wait(&status)) > 0) {
+		if(WEXITSTATUS(status) == FOUND) {
+			printf("Hash found\n");
+			for(int i = 0; i < threads; i++) {
+				kill(running[i], SIGKILL);
+			}
+		}
+
+		printf("%d end value: %d\n", pid, WEXITSTATUS(status));
+
+	}
 
 
 	return 0;
